5fadf439e8aed68abf9b504381cf7ea474b7841f
Abrish-21 <abrham.tilksew@aastustudent.edu.et>
2024-11-15 11:24:47 -0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import HotelBg from './assets/images/ResortBg.jpg'
+
 
 function App() {
   return (
-    <div>This is Dink Hotel Management System</div>
+    <main>
+      <header>
+       <p>NavBar</p>
+      </header>
+      <section className='h-screen'>
+        
+        <img src={HotelBg} alt="" className='rounded-lg' />
+      </section>
+      <section>
+        <p>About US</p>
+      </section>
+      <section>
+        <p>Our Facilities</p>
+      </section>
+      <section>
+        <p>Our Rooms</p>
+      </section>
+      <section>
+        <p>Coral Island</p>
+      </section>
+      <section>
+        <p>Review</p>
+      </section>
+      <section>
+        <p>Hotel-Map</p>
+      </section>
+      <section>
+        <p>Footer</p>
+      </section>
+    </main>
   )
 }
 
